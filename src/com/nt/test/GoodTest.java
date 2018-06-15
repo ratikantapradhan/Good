bb3cd1bf5793153f86c3cee97b26e48fab57de5a
RATIKANTA PRADHAN <RATIKANTA PRADHAN@RATIKANTA-PC>
2018-06-15 21:30:38 +0530

    dsrty

--- src/com/nt/test/GoodTest.java
+++ src/com/nt/test/GoodTest.java
@@ -5,4 +5,29 @@ private String ename;
 private int sno;
 private int roll;
 private String no;
+public String getEname() {
+	return ename;
+}
+public void setEname(String ename) {
+	this.ename = ename;
+}
+public int getSno() {
+	return sno;
+}
+public void setSno(int sno) {
+	this.sno = sno;
+}
+public int getRoll() {
+	return roll;
+}
+public void setRoll(int roll) {
+	this.roll = roll;
+}
+public String getNo() {
+	return no;
+}
+public void setNo(String no) {
+	this.no = no;
+}
+
 }
